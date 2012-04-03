48960cecbe3263630b447ae1fa94b339e5377aeb
bill <bill@left.(none)>
2012-04-03 02:31:54 -0700

    sha256 example

---
+++ src/sha256-test.go
@@ -0,0 +1,44 @@
+
+package main
+
+import(
+    "fmt"
+    "crypto/sha256"
+	"os"
+	"bufio"
+	"flag" 
+//	"bytes"
+//    "hash"
+)
+
+var plaintext string
+
+func main() {
+
+	var ( 
+		filename string
+		file *os.File
+		count int
+		err error
+	)
+	flag.Parse()
+	if flag.NArg() > 0 {
+        filename = flag.Arg(0)
+    } else { 
+		filename = "1MB"
+	}
+
+	if file, err = os.Open(filename); err != nil {
+        return
+    }
+	reader:= bufio.NewReader(file)
+	buffer:= make ([]byte,16384)
+    h := sha256.New()  // h is a hash.Hash
+	for {
+		if count, err = reader.Read(buffer); err != nil {
+			break
+		}
+    	h.Write(buffer[:count])
+	}
+    fmt.Printf("%x  %s\n", h.Sum(nil),filename)
+}
