0ad6f5c961b7fd6dfe2a6e55de3f042b868bc11e
spikebike <bill@broadley.org>
2012-04-03 02:39:12 -0700

    format tweak

--- src/sha256-test.go
+++ src/sha256-test.go
@@ -1,12 +1,12 @@
-
 package main
 
-import(
-    "fmt"
-    "crypto/sha256"
-	"os"
+import (
 	"bufio"
-	"flag" 
+	"crypto/sha256"
+	"flag"
+	"fmt"
+	"os"
+
 //	"bytes"
 //    "hash"
 )
@@ -15,30 +15,30 @@ var plaintext string
 
 func main() {
 
-	var ( 
+	var (
 		filename string
-		file *os.File
-		count int
-		err error
+		file     *os.File
+		count    int
+		err      error
 	)
 	flag.Parse()
 	if flag.NArg() > 0 {
-        filename = flag.Arg(0)
-    } else { 
+		filename = flag.Arg(0)
+	} else {
 		filename = "1MB"
 	}
 
 	if file, err = os.Open(filename); err != nil {
-        return
-    }
-	reader:= bufio.NewReader(file)
-	buffer:= make ([]byte,16384)
-    h := sha256.New()  // h is a hash.Hash
+		return
+	}
+	reader := bufio.NewReader(file)
+	buffer := make([]byte, 16384)
+	h := sha256.New() // h is a hash.Hash
 	for {
 		if count, err = reader.Read(buffer); err != nil {
 			break
 		}
-    	h.Write(buffer[:count])
+		h.Write(buffer[:count])
 	}
-    fmt.Printf("%x  %s\n", h.Sum(nil),filename)
+	fmt.Printf("%x  %s\n", h.Sum(nil), filename)
 }
