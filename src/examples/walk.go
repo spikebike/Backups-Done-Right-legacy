dbe92a1b2a9b720970aae796687a211f2acd12d7
Tectu <joel@unormal.org>
2012-04-08 13:58:27 +0200

    walker.go does accept any path now througth argument. default argument is "."

--- src/examples/walk.go
+++ src/examples/walk.go
@@ -4,9 +4,12 @@ import (
 	"os"
 	"path/filepath"
 	"log"
+	"flag"
 )
 
 func main() {
+	flag.Parse()
+
 	markFn := func(path string, info os.FileInfo, err error) error {
 		if path == "doozerd" { // Will skip walking of directory pictures and its contents.
 			log.Println("Skipping",path)
@@ -18,7 +21,13 @@ func main() {
 		log.Println(path)
 		return nil
 	}
-	err := filepath.Walk(".", markFn)
+
+	path := "."
+	if flag.Arg(0) != "" {
+		path = flag.Arg(0)
+	}
+
+	err := filepath.Walk(path, markFn)
 	if err != nil {
 		log.Fatal(err)
 	}
