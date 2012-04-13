9bcf0a4349af22e88c54df3ccdc2200c9f0a2f6b
Tectu <joel@unormal.org>
2012-04-13 17:44:57 +0200

    changed order in main(). makes more sense for debugging

--- src/walker.go
+++ src/walker.go
@@ -13,13 +13,12 @@ import (
 )
 
 var (
-	configFile = flag.String("config", "../etc/config.cfg", "Defines where to load configuration from")
-
 	sqls = []string {
 		"create table dirs (id INTEGER PRIMARY KEY,st_mode INT, st_ino BIGINT, st_uid INT, st_gid INT, name varchar(2048), last_seen ts, deleted INT)",
 		"create table files (id INTEGER PRIMARY KEY, st_mode INT, st_ino BIGINT, st_dev BIGINT, st_nlink INT, st_uid INT, st_gid INT, st_size BIGINT, st_atime BIGINT, st_mtime BIGINT, st_ctime BIGINT, name varchar(255), dirID BIGINT, last_seen ts, deleted INT, FOREIGN KEY(dirID) REFERENCES dirs(id))",
 	}
 
+	configFile = flag.String("config", "../etc/config.cfg", "Defines where to load configuration from")
 )
 
 func init_db(dataBaseName string) (db *sql.DB, err error) {
@@ -75,12 +74,16 @@ func backupDir(db *sql.DB, dirList string) error {
 
 func main() {
 	flag.Parse()
-	config, _ := config.ReadDefault(*configFile)
+
 	log.Printf("Loading config file from %s\n", *configFile)
-	dirList, _ := config.String("Client", "backup_dirs_secure")
+	config, _ := config.ReadDefault(*configFile)
+
 	log.Printf("Backing up these directories: %s\n", dirList)
-	dataBaseName, _ := config.String("Client", "sql_file")
+	dirList, _ := config.String("Client", "backup_dirs_secure")
+
 	log.Printf("Attempting to open %s", dataBaseName)
+	dataBaseName, _ := config.String("Client", "sql_file")
+
 	db, err := init_db(dataBaseName)
 	err = backupDir(db, dirList)
 	log.Printf("backupDir exited with %s", err)
