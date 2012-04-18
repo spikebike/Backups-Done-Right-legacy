ac104b34ec93b2ed4ce58e86f17d41bd9fe7ece9
spikebike <bill@broadley.org>
2012-04-17 18:53:37 -0700

    MUCH improved implementation of REQUEST for bdr-proto

--- src/bdr-proto/test.go
+++ src/bdr-proto/test.go
@@ -6,54 +6,30 @@ import (
 	"fmt"
 )
 
-import M "math/rand"
+//import M "math/rand"
 import C "crypto/rand"
 
 func main() {
-	// not sure how to create more than 1 blob message within a single request.  
-	// The bdr_proto.prof file allows it.  The protobufs comments at
-	// http://code.google.com/p/protobuf/source/browse/trunk/src/google/protobuf/message.h
-	// seem to claim something like bdr_proto.Add_RequestBlob should work 
-	// the bdr_proto/bdr_proto.pb.go makes no mention of add.
-	var s1 int32
-	var i int32
-	var intptr *int32
-	var strptr *string
-	var randBytes []byte
-	str1 := "Hello"
-	s1 = 1024
-
-	// I had hoped just repeating the records would work.
-	//
-	//	t1:= &bdr_proto.RequestBlob{Sha256: &str1, Bsize: &s1, Sha256: &str2, Bsize: &s2}
-	// build an array of 32 RequestBlob
-	Blobarray := make([]bdr_proto.RequestBlob, 32)
-	//* build an array of Requestblob pointers
-	BlobarrayPtr := make([]*bdr_proto.RequestBlob, 32)
-	t1 := new(bdr_proto.Request)
-	t1.Blobarray = BlobarrayPtr
-	for i = 0; i < 32; i++ {
+	req := &bdr_proto.Request{}
+	for i := 0; i < 8; i++ {
 		// read in 16 bytes from /dev/urandom to sha256
-		randBytes = make([]byte, 16)
-		_, _ = C.Read(randBytes)
+		randBytes := make([]byte, 16)
+		C.Read(randBytes)
+
+		// get its size
+		size := int32(len(randBytes))
+
 		// create a new hash, and do a crypty hash of the random bytes.
-		hash := sha256.New()
-		_, _ = hash.Write(randBytes)
-		// build a new string and store the hash in it
-		strptr = new(string)
-		*strptr = fmt.Sprintf("%x", hash.Sum(nil))
-		/* point each array to the address of the invidual blob */
-		t1.Blobarray[i] = &Blobarray[i]
-		/* for that blob set the sha256 to the address of the new sha256 */
-		t1.Blobarray[i].Sha256 = strptr
-		/* create a new int32, store a random number in it, and assign
-		it to bsize */
-		intptr = new(int32)
-		*intptr = M.Int31()
-		t1.Blobarray[i].Bsize = intptr
-		fmt.Printf("i= %2d size= %10d sha256=%s\n", i, *intptr, *strptr)
+		sha := sha256.New()
+		sha.Write(randBytes)
+		strhash := fmt.Sprintf("%x", sha.Sum(nil))
+		fmt.Printf("i=%d sha=%x size=%d\n", i,strhash,size)
+		
+		req.Blobarray = append(req.Blobarray, &bdr_proto.RequestBlob{Sha256: &strhash, Bsize: &size})
 	}
-	t2 := &bdr_proto.RequestBlob{Sha256: &str1, Bsize: &s1}
-	fmt.Printf("t1 = %+v\n", t1)
-	fmt.Printf("t2 = %+v\n", t2)
+	fmt.Printf("%#V %T\n", req, req)
+	for i := 0; i < 8; i++ {
+		fmt.Printf("%s %d \n", *req.Blobarray[i].Sha256, *req.Blobarray[i].Bsize)
+	}
+
 }
