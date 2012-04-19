e75ab4fd3833440ecd704f2e3282d616d268d4f7
spikebike <bill@broadley.org>
2012-04-19 02:57:12 -0700

    server fixed, thanks smw

--- src/upload-client.go
+++ src/upload-client.go
@@ -37,5 +37,5 @@ func main() {
 	if err := request.Request(req, ack); err != nil {
 		log.Fatalf("Add failed with: %s", err)
 	}
-	fmt.Printf("Received %d\n\n", *ack.Received)
+	fmt.Printf("Server ACKs %d records\n\n", *ack.Received)
 }
--- src/upload-server.go
+++ src/upload-server.go
@@ -11,15 +11,15 @@ import (
 type Request struct{}
 
 func (Request) Request(in *bdrservice.RequestMessage, out *bdrservice.RequestACKMessage) error {
-	log.Printf("server: blobarray=%v %T", *in.Blobarray[0].Sha256,*in.Blobarray[0].Sha256)
-	log.Printf("server: blobarray=%v %T", *in.Blobarray[0],*in.Blobarray[0])
-//	for _, i := range in.Blobarray[] { 
-//		log.Printf("server: blobarray=%v %T", *in.Blobarray[i].Sha256,*in.Blobarray[i].Sha256)
-//	}
+	var records int32
+	for _, i := range in.Blobarray { 
+		log.Printf("server: blobarray=%v %T", *i.Sha256,*i.Sha256)
+		records++
+	}
 
 	
 	out.Received = new(int32)
-	*out.Received = 5 
+	*out.Received =  records 
 	return nil
 }
 
