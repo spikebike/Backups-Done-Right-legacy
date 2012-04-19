cf729518525512f4f401ce23bf430e05b125dd33
spikebike <bill@broadley.org>
2012-04-19 04:43:16 -0700

    function-passing

---
+++ src/examples/function-passing.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+// return a function 
+func Sequence() func() int {
+	n := 0
+	return func() int {
+		n++
+		return n
+	}
+}
+
+// take a funtion as argument 
+func Print(f func() int) {
+	fmt.Printf("%d\n", f())
+}
+
+func main() {
+	seq := Sequence()
+
+	fmt.Printf("%d\n", seq())
+	fmt.Printf("%d\n", seq())
+
+	Print(seq)
+}
