8b8e6193309088381bf209f4eaaa5072bd211149
spikebike <bill@broadley.org>
2012-04-19 01:41:22 -0700

    client cleanup

--- src/examples/client-server-tls-proto/add.go
+++ src/examples/client-server-tls-proto/add.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"./addservice"
-	"crypto/rand"
-	"crypto/tls"
-	"crypto/x509"
+	"./tlscon"
 	"flag"
 	"log"
 	"net"
@@ -27,65 +25,12 @@ func (Add) Add(in *addservice.AddMessage, out *addservice.SumMessage) error {
 	return nil
 }
 
-func handleClient(conn net.Conn, f func(conn net.Conn)) {
-	tlscon, ok := conn.(*tls.Conn)
-	if ok {
-		log.Print("server: conn: type assert to TLS succeedded")
-		err := tlscon.Handshake()
-		if err != nil {
-			log.Fatalf("server: handshake failed: %s", err)
-		} else {
-			log.Print("server: conn: Handshake completed")
-		}
-		state := tlscon.ConnectionState()
-		// Note we could reject clients if we don't like their public key.		
-		log.Println("Server: client public key is:")
-		for _, v := range state.PeerCertificates {
-			log.Print(x509.MarshalPKIXPublicKey(v.PublicKey))
-		}
-		// Now that we have completed SSL/TLS 
-		// hopefully F does the same as below
-		f(conn)
-		//		addservice.ServeAddService(tlscon, Add{})
-	}
-}
-
-func serverTLSListen(service string, f func()) {
-
-	// Load x509 certificates for our private/public key, makecert.sh will
-	// generate them for you.
-
-	cert, err := tls.LoadX509KeyPair("certs/server.pem", "certs/server.key")
-	if err != nil {
-		log.Fatalf("server: loadkeys: %s", err)
-	}
-	// Note if we don't tls.RequireAnyClientCert client side certs are ignored.
-	config := tls.Config{Certificates: []tls.Certificate{cert}, ClientAuth: tls.RequireAnyClientCert}
-	config.Rand = rand.Reader
-	listener, err := tls.Listen("tcp", service, &config)
-	if err != nil {
-		log.Fatalf("server: listen: %s", err)
-	}
-	log.Print("server: listening")
-	// Keep this loop simple/fast as to be able to handle new connections
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Printf("server: accept: %s", err)
-			break
-		}
-		log.Printf("server: accepted from %s", conn.RemoteAddr())
-		// Fire off go routing to handle rest of connection.
-		go handleClient(conn, f())
-	}
-}
-
 func AddFunc(conn net.Conn) {
 	addservice.ServeAddService(conn, Add{})
 }
 
 func main() {
 	flag.Parse()
-	fptr := AddFunc()
-	serverTLSListen("0.0.0.0:8000", fptr())
+	fptr := AddFunc
+	tlscon.ServerTLSListen("0.0.0.0:8000", fptr)
 }
