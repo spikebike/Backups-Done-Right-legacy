5c92aacd0157823d90fee37cd9548d873b844313
Tectu <joel@unormal.org>
2012-04-21 14:52:25 +0200

    fixed benchSql.sh

--- src/examples/benchSql.sh
+++ src/examples/benchSql.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for i in 20 40 80 160 320 640; do go run sqlite-performacetest.go 10000 $i; done | grep seconds
+for i in 20 40 80 160 320 640; do go run sqlite-perf.go 10000 $i; done | grep seconds
