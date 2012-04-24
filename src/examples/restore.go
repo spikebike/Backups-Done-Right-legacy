340db50478b8b32207aa51f579a57666076386ca
Tectu <joel@unormal.org>
2012-04-24 22:50:54 +0200

    save commit for restore.go - not finished yet

--- src/examples/restore.go
+++ src/examples/restore.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"../bdrsql"
 	)
 
+const DataBaseName = "./example-db.sql"
+
 func main() {
-	fmt.Println("restore example")
+	db, err := bdrsql.Init_db(DataBaseName, false);
+	if err != nil {
+		fmt.Println("coudln't open database. Error: %s", err)
+	} else {
+		fmt.Println("opened database")
+	}
+	defer db.Close()
+
+	stmt, err := db.Prepare("select name from files where id = ?")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer stmt.Close()
+
+	var name string
+	err = stmt.QueryRow("1").Scan(&name)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("id 1 name %s", name)
 }
