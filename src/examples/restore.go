a2a5c2c25f7e4f7f3d33ba44d2b83a4a37419d59
Tectu <joel@unormal.org>
2012-04-25 10:37:28 +0200

    restore.go process

--- src/examples/restore.go
+++ src/examples/restore.go
@@ -3,9 +3,58 @@ package main
 import (
 	"fmt"
 	"../bdrsql"
+	"database/sql"
 	)
 
 const DataBaseName = "./example-db.sql"
+
+type file_info_t struct {
+	id int
+	mode int
+	ino int
+	dev int
+	nlink int
+	uid int
+	gid int
+	size int64
+	atime int
+	mtime int
+	ctime int
+	name string
+	dirID int
+	last_seen int
+	deleted int
+}
+
+func printStruct(f *file_info_t) {
+	fmt.Println("name: ", f.name)
+	fmt.Println("uid: ", f.uid)
+	fmt.Println("gid: ", f.gid)
+	fmt.Println("size: ", f.size)
+	fmt.Println("mtime: ", f.mtime)
+	fmt.Println("atime: ", f.atime)
+	fmt.Println("ctime: ", f.ctime)
+}
+
+func readFileInfo(db *sql.DB, fname string) *file_info_t{
+	f := &file_info_t{}
+
+        stmt, err := db.Prepare("select mode, ino, uid, gid, size, mtime, atime, ctime from files where name = ?")
+        if err != nil {
+                fmt.Println(err)
+                return nil
+        }
+        defer stmt.Close()
+
+	err = stmt.QueryRow(fname).Scan(&f.mode, &f.ino, &f.uid, &f.gid, &f.size, &f.mtime, &f.atime, &f.ctime)
+        if err != nil {
+                fmt.Println(err)
+                return nil
+        }
+	f.name = fname
+
+	return f
+}
 
 func main() {
 	db, err := bdrsql.Init_db(DataBaseName, false);
@@ -16,18 +65,6 @@ func main() {
 	}
 	defer db.Close()
 
-	stmt, err := db.Prepare("select name from files where id = ?")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer stmt.Close()
-
-	var name string
-	err = stmt.QueryRow("1").Scan(&name)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("id 1 name %s\n", name)
+	f := readFileInfo(db, "avr.odt")
+	printStruct(f)
 }
