b84bc54b2ad685b76a34dafae39fbc87d39d8323
spikebike <bill@broadley.org>
2012-05-10 23:55:36 -0700

    fixed makecert.sh and put it in src

---
+++ src/makecert.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# call this script with an email address (valid or not).
+# like:
+# ./makecert.sh joe@random.com
+if [ ! "$#" -eq 1 ];then
+	echo "You MUST include an email address as an argument"
+	exit;
+fi
+if [ ! -d certs ]; then
+	mkdir certs
+fi
+rm certs/*
+echo "make server cert"
+openssl req -new -nodes -x509 -out certs/server.pem -keyout certs/server.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Random Company/OU=IT/CN=www.random.com/emailAddress=$1"
+echo "make client cert"
+openssl req -new -nodes -x509 -out certs/client.pem -keyout certs/client.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Random Company/OU=IT/CN=www.random.com/emailAddress=$1"
