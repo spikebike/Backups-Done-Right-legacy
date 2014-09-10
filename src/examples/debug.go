e74a5e4c7b8985d18be3ece786ccdb1f71916928
Bill Broadley <bill@cse.ucdavis.edu>
2014-09-10 01:02:17 -0700

    more debugging and gofmt fix

--- src/examples/debug.go
+++ src/examples/debug.go
@@ -17,29 +17,38 @@ const (
 	delaySeconds = 10
 )
 
-func TestStress(sig chan os.Signal) {
+func HandleSignals(sig chan os.Signal) {
+	// this loop runs once per signal
 	for ss := range sig {
 		fmt.Printf("received: %s\n", ss)
 		fmt.Printf("iAmAnImportantNumber=%s\n", iAmAnImportantNumber.String())
+		fmt.Printf("totalClients=%s\n", totalClients.String())
+		fmt.Printf("totalRequests=%s\n\n", totalRequests.String())
 	}
 }
 
 func main() {
+	// launch webserver to serve out debugging variables
 	go http.ListenAndServe(fmt.Sprintf(":%d", httpPortTCP), nil)
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, syscall.SIGUSR1)
-	go TestStress(sig)
 
-	fmt.Printf("Go to http://localhost:%d/debug/vars in the next %d seconds \n", httpPortTCP, delaySeconds)
-	fmt.Printf("my PID=%d, kill -SIGUSR1 %d if you want\n", syscall.Getpid(), syscall.Getpid())
+	// make channel for sending signals
+	sig := make(chan os.Signal, 1)
+	// send a singal on channel cig for each SIGUSR1
+	signal.Notify(sig, syscall.SIGUSR1)
+	// run signal handler in gorouting
+	go HandleSignals(sig)
+
+	fmt.Printf("\nGo to http://localhost:%d/debug/vars in the next %d seconds \n", httpPortTCP, delaySeconds)
+	fmt.Printf("my PID=%d, kill -SIGUSR1 %d if you want\n\n", syscall.Getpid(), syscall.Getpid())
 	time.Sleep(delaySeconds * time.Second)
 	fmt.Print("waited 10, sending SIGUSR1, waiting another 10 seconds\n")
 	syscall.Kill(syscall.Getpid(), syscall.SIGUSR1)
 	time.Sleep(delaySeconds * time.Second)
+	fmt.Print("Done waiting, goodbye\n")
 }
 
 var (
 	iAmAnImportantNumber = expvar.NewInt("iAmAnImportNumber")
 	totalClients         = expvar.NewInt("totalClients")
-	totalRequsts         = expvar.NewInt("totalRequests")
+	totalRequests        = expvar.NewInt("totalRequests")
 )
